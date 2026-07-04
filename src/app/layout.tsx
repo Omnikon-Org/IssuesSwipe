19c6c5020ebdf4d2c1f3fac877cb62c7181c376f
skullxcode <pranavthawait@gmail.com>
2026-07-05 00:22:12 +0530

    popup

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
+import InstallPrompt from "@/components/InstallPrompt";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -54,6 +55,7 @@ export default function RootLayout({
       </head>
       <body className="min-h-full bg-dark-bg text-foreground antialiased flex flex-col font-sans">
         {children}
+        <InstallPrompt />
       </body>
     </html>
   );
